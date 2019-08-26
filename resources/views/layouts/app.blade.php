02ac09916984b9f1d57815e9343b3102c553df2f
hoang-dinh <https://proto-itsd.backlog.jp>
2019-08-26 17:48:55 +0700

    done master template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>To Do List</title>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<!-- Navbar Contents -->
+		</nav>
+	</div>
+	@yield{'content'};
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Show Task Dashboard
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Add new Task
+Route::post('/task', function() {
+    //
+});
+
+//Delete Task
+Route::delete('/task/{task}', function(Task $task) {
+    //
+});
